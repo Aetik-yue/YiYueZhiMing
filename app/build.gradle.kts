0885b5facc16a4a9df0c262830e9142c7596bcb0
Aetik <yanhaoruiruihaoyan@163.com>
2026-07-22 00:45:54 +0800

    Release 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.yiyuezhiming"
         minSdk = 26
         targetSdk = 36
-        versionCode = 112
-        versionName = "1.1.2"
+        versionCode = 113
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
